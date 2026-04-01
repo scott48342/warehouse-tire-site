abeb121e7c24c6add8301faa479d5ab19a1c41f1
Scott <scott@warehousetire.net>
2026-04-01 09:12:15 -0400

    Add Google Ads tag (AW-410517185)

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -3,16 +3,21 @@
 import Script from 'next/script'
 
 const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+const GOOGLE_ADS_ID = 'AW-410517185'
 
 export function GoogleAnalytics() {
-  if (!GA_MEASUREMENT_ID) {
+  // Always render if we have either GA or Google Ads
+  if (!GA_MEASUREMENT_ID && !GOOGLE_ADS_ID) {
     return null
   }
+
+  // Use GA ID for script loading, fall back to Ads ID
+  const primaryId = GA_MEASUREMENT_ID || GOOGLE_ADS_ID
 
   return (
     <>
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${primaryId}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -20,9 +25,8 @@ export function GoogleAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}', {
-            page_path: window.location.pathname,
-          });
+          ${GA_MEASUREMENT_ID ? `gtag('config', '${GA_MEASUREMENT_ID}', { page_path: window.location.pathname });` : ''}
+          gtag('config', '${GOOGLE_ADS_ID}');
         `}
       </Script>
     </>
