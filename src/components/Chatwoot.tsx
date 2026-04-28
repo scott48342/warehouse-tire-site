ab1ae29eb8baee2b1a5e151e1a85cfe601751790
Scott <scott48340@gmail.com>
2026-04-28 08:33:03 -0400

    fix: revert BASE_URL to working l388 instance

--- src/components/Chatwoot.tsx
+++ src/components/Chatwoot.tsx
@@ -15,7 +15,7 @@ export function Chatwoot() {
             launcherTitle: "Chat with us"
           };
           (function(d,t) {
-            var BASE_URL = "https://chatwoot-web.onrender.com";
+            var BASE_URL = "https://chatwoot-web-l388.onrender.com";
             var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
             g.src = BASE_URL + "/packs/js/sdk.js";
             g.defer = true;
